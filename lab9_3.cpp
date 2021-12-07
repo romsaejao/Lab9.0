64f14b015cb551d116af68a9bbc4c2defa26f285
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-12-07 22:41:46 +0700

    Update lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,61 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age, h;
+    long long int p;
+    cout<<"Enter your age: ";
+    cin>>age;
+    
+    
+    if(age<=20)
+    {
+        cout<<"Enter yout height: ";
+        cin>>h;
+        if(h<160)
+        {
+            cout<<"Your status = UNFRIEND";
+        }
+        else
+        {
+            if(h<175)
+            {
+                cout<<"Your status = FRIEND";
+            }
+            else
+            {
+                cout<<"Enter your property: ";
+                cin>>p;
+            if(p>69000000)
+            {
+                cout<<"Your status = MARRIED";
+            }
+            else
+            {
+                cout<<"Your status = ONE-NIGHT-STAND";
+            }
+        }
+    }
+    }
+    else if(age<=30)
+    {
+        cout<<"Enter your property: ";
+        cin>>p;
+        
+        if(p>10000000)
+        {
+            cout<<"Your status = BEST FRIEND";
+        }
+        else
+        {
+            cout<<"Your status = UNFRIEND";
+        }
+        
+    }
+    if(age>=30)
+    {
+            cout<<"Your status = UNFRIEND";
+    }
+}
+
+
